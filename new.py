157f84a48f63e29fd25be110f9c28998231bf09e
Deepika Upadhyay <deepikaupadhyay01@gmail.com>
2017-04-16 12:53:21 +0530

    learnt to commit with doc change

--- new.py
+++ new.py
@@ -1,15 +1,37 @@
-import shelve
-s = shelve.open("mydict.db")
-try:
-s["mydict"] = { 1:'foo',2:'bar',3:'lumberjack'}
-finally:
-    s.close()
+def main():
+ import shelve
+ s = shelve.open("drows.db")
+ try:
+     delrow(s, 'foo')
+     delrow(s, 'bar')
+     delrow(s, 0)
 
-    s = shelve.open("mydict.db")
+     s['0'] =  { 1:'foo',2:'bar',3:'lumberjack'}
+     s["Hello"] =  { 1:'foo',2:'bar',3:'lumberjack'}
+     s['Spam'] =  { 1:'foo',2:'bar',3:'lumberjack'}
+
+              
+                  
+ finally:
+   s.close()
+
+ s = shelve.open("drows.db") 
+
+
+ for item in s:
+     if item != '0':
+      print(item + ": " + str(s[item]))
+
+def delrow(s, rowkey):
+  try:
+      del s[rowkey]
+  except:
+         pass
     
-    mydict = s["mydict"]
 
-for item in mydict:
-print(mydict[item])
+
+if __name__ == "__main__":
+  main()
+
 
                                                                                                                                                                                 
