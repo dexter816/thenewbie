fe41cc6b4cdf911746a4cda385ca3a3228690686
Deepika Upadhyay <deepikaupadhyay01@gmail.com>
2017-04-16 12:25:14 +0530

    this is modification to the dictionary

--- new.py
+++ new.py
@@ -1,5 +1,15 @@
-mydict = { 1:'foo',2:'bar',3:'lumberjack'};
+import shelve
+s = shelve.open("mydict.db")
+try:
+s["mydict"] = { 1:'foo',2:'bar',3:'lumberjack'}
+finally:
+    s.close()
+
+    s = shelve.open("mydict.db")
+    
+    mydict = s["mydict"]
+
 for item in mydict:
 print(mydict[item])
 
-
+                                                                                                                                                                                
